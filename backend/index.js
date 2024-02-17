81e8a1b9066dc0e8b5306c41c3654d6ce97e79cb
Vinay Jose <59995160+vinay-jose@users.noreply.github.com>
2024-02-17 15:37:59 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
+const cors = require('cors');
 const express = require("express");
 const app = express();
 const port = 3001;
-var cors = require('cors');
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 const dishes = require("./routes/dishesRoutes")
